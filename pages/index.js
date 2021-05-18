6e07cf459c8efc8fd1d9f3b3fe528e112fcb82d6
Andrew Li <54134293+andrewli26@users.noreply.github.com>
2021-05-18 20:18:30 +0800

    Base for home page
    move to linux

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,65 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/mun.module.css'
+import React from 'react'
+
+class HugeImage extends React.Component {
+  render() {
+    return (
+      <img className={styles.image} alt={this.props.alt} src={this.props.src} width="100%" height="100%"/>
+    )
+  }
+}
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <div class={styles.bar}>
+        <span>Dropdown 1</span>
+      </div>
+    )
+  }
+}
+
+class Bar extends React.Component {
+  render() {
+    return (
+      <div class={styles.bar}>&nbsp;</div>
+    )
+  }
+}
 
 export default function Home() {
+  let dataSource = [];
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>THISMUN 2022</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div id="top">
+          <HugeImage src="BACKGROUND.jpg"/>
+          <div className={styles.i19}><h1 className={styles.whitetitle}>
+            Welcome to <span className={styles.accent3}>THISMUN 2022</span>
+          </h1></div>
+      </div>
+      <NavigationBar/>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">THISMUN 2022</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <img src="EXECS.jpeg" alt="Image of execs/leaders/idk"/>
+          <h1 className={styles.welchead}>Welcome to THISMUN!</h1>
+          <p className={styles.welcome}>Welcome letter goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dolor sed urna ullamcorper egestas nec tincidunt enim. Nullam sed ultrices mauris, sed aliquam eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia risus risus, sed elementum quam rhoncus sit amet. Praesent vel dignissim metus. Sed ante ex, volutpat ac rutrum vel, dapibus ac nisi. Donec vel mauris et mi venenatis accumsan. Maecenas ultrices dui at varius condimentum. Proin euismod ullamcorper porta. Nulla facilisi. Aenean in sem pulvinar eros facilisis fringilla. In ac lectus rhoncus, porta diam quis, congue tortor. Duis tincidunt ornare augue, sollicitudin semper augue malesuada ut. Morbi id porta metus. Curabitur at ipsum congue, congue elit eu, consequat est. Etiam euismod imperdiet urna, non scelerisque odio sagittis sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam ornare tortor id euismod lacinia.</p>
+          <h1 className={styles.welcsubhead}>Sincerely,</h1>
+          <p className={styles.welcome}>Names go here. Lorem, Ipsum, Dolar, and Sit.</p>
+          <p className={styles.welcome}>Contact our advisor at <a href="mailto:bsumita@this.edu.cn">bsumita@this.edu.cn</a></p>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className={styles.footerbg}>
+        <center className={styles.padcent}>
+          <img src="THISMUNLOGO.png" width="100px" height="100px"/>
+          <img src="SCHOOL LOGO.png" width="100px" height="100px"/>
+          <p className={styles.addressheading}><a href="http://www.this.edu.cn">Tsinghua International School</a></p>
+          <p className={styles.address}>Campus of Tsinghua High School, Beijing, China, 100084</p>
+        </center>
+      </div>
     </div>
   )
 }
