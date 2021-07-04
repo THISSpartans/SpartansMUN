99c54306c9a7f842fad76263bcd8ee7b2769ffa5
Andrew Li <54134293+andrewli26@users.noreply.github.com>
2021-07-04 18:33:22 +0800

    logo

--- pages/src/header.js
+++ pages/src/header.js
@@ -1,13 +1,15 @@
 import styles from '../../styles/Cycles.module.css'
 import React, { useState, Fragment } from 'react'
 import Fade from 'react-reveal/Fade';
-import Navigation from './navigation.js'
+import Navigation from './navigation.js';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export class Header extends React.Component {
   render() {
     return (
       <div className={styles.header}>
-        <a href="/"><div className={styles.logo}><img src="THISMUNLOGO.png"/></div></a>
+        <a href="/"><div className={styles.logo}><LazyLoadImage effect="blur" src="THISMUNLOGO.png"/></div></a>
         <a href="/"><div className={styles.ltext}>THIS<span id={styles.m}>MUN</span>&nbsp;I
         <div className={styles.btext}><em>Fostering Global Ethics in an Age of Post Truths</em></div></div></a>
         <Navigation/>
